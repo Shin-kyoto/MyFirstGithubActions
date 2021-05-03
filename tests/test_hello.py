6d1163c6e432c7935e69c4d640f5f7cd2c0252be
Shin-kyoto <aquashin0202@gmail.com>
2021-05-03 16:37:30 +0900

    Modify path

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,7 +1,14 @@
 import sys
+import pathlib
+# base.pyのあるディレクトリの絶対パスを取得
+current_dir = pathlib.Path(__file__).resolve().parent
+parent_dir = current_dir.joinpath('..')
+# モジュールのあるパスを追加
+sys.path.append( str(parent_dir) )
 
-sys.path.insert(0,'../')
-
+# sys.path.insert(0,'../')
+# sys.path.append('../')
+# print(sys.path)
 from hello import hello
 
 def test_hello_default(capsys):
